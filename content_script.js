cd92906284c23b944946b0ba448480e2eaf6923a
Aravind-Suresh <arvindsuresh2009@gmail.com>
2016-07-30 13:38:15 +0530

    click-events recorded and triggered.

--- content_script.js
+++ content_script.js
@@ -2,7 +2,7 @@
 * @Author: aravind
 * @Date:   2016-07-29T22:44:05+05:30
 * @Last modified by:   aravind
-* @Last modified time: 2016-07-30T13:02:01+05:30
+* @Last modified time: 2016-07-30T13:31:02+05:30
 */
 
 var tasks = [];
@@ -20,19 +20,33 @@ function recordDOMEvents() {
 };
 
 function stopRecordingDOMEvents() {
-  console.log(tasks);
   events.forEach(function(eventName, idx) {
     document.removeEventListener(eventName, eventHandler);
   });
 };
 
+function generateEvent(event) {
+  var t = event.type, e;
+  if (t == 'click') {
+    e = $.Event(t);
+  } else if (t == 'keypress') {
+    e = $.Event(t);
+    e.keyCode = event.keyCode;
+    e.which = event.keyCode;
+  }
+  e.target = event.target;
+  return e;
+}
+
 function dispatchTasks() {
-  console.log(tasks);
   tasks.forEach(function(event, idx) {
-    var element = $(event.target);
-    console.log("Trigger:", event);
-    element.trigger(event);
+    var _element = $(event.target);
+    _element.focus();
+    var _event = generateEvent(event);
+    _element.trigger(_event);
+    console.log("Trigger:", _event);
   });
+  tasks = [];
 };
 
 chrome.runtime.onMessage.addListener(
